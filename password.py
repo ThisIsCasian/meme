29ff0a059d1fe9d1c0445640d081922751434739
ThisIsCasian <81656666+ThisIsCasian@users.noreply.github.com>
2026-02-23 15:28:23 +0100

    Print greeting message in password.py
    Added a greeting message before password generation.

--- password.py
+++ password.py
@@ -2,7 +2,7 @@ import random
 
 elementi = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 l = int(input("Dammi la lunghezza: "))
-
+print("ciao")
 password = ""
 
 for i in range(l):
